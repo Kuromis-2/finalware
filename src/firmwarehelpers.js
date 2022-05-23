60ce8e20b8e4f5131ff814c7f739a0f6d38a444a
Christoph Tillschneider <christoph.tillschneider@gmail.com>
2022-05-23 18:45:58 +0200

    logging and network error handling

--- src/firmwarehelpers.js
+++ src/firmwarehelpers.js
@@ -4,146 +4,163 @@ const https = require("https");
 const fs = require("fs");
 const axios = require("axios");
 const path = require("path");
-
+const log = require("electron-log");
 function getAppDataPath() {
-    switch (process.platform) {
-      case "darwin": {
-        return path.join(process.env.HOME, "Library", "Application Support", "Finalware");
-      }
-      case "win32": {
-        return path.join(process.env.APPDATA, "Finalware");
-      }
-      case "linux": {
-        return path.join(process.env.HOME, ".Finalware");
-      }
-      default: {
-        console.log("Unsupported platform!");
-        process.exit(1);
-      }
+  switch (process.platform) {
+    case "darwin": {
+      return path.join(process.env.HOME, "Library", "Application Support", "Finalware");
     }
+    case "win32": {
+      return path.join(process.env.APPDATA, "Finalware");
+    }
+    case "linux": {
+      return path.join(process.env.HOME, ".Finalware");
+    }
+    default: {
+      console.log("Unsupported platform!");
+      process.exit(1);
+    }
+  }
 }
 
 const APP_DATA_PATH = getAppDataPath();
 const FIRMWARE_PATH = path.join(APP_DATA_PATH, "firmware.zip");
 
 async function downloadFirmware(firmwareVersion) {
-    if (!firmwareVersion) throw new Error("firmwareVersion is null");
-    const firmwareLookup =
-        "https://raw.githubusercontent.com/Kuromis-2/newest-firmware/main/firmwarelookup.json";
+  if (!firmwareVersion) throw new Error("firmwareVersion is null");
+  const firmwareLookup =
+    "https://raw.githubusercontent.com/Kuromis-2/newest-firmware/main/firmwarelookup.json";
 
-    // Use axios to fetch data from firmwareLookup
-    const { data } = await axios.get(firmwareLookup);
+  // Use axios to fetch data from firmwareLookup
+  const { data } = await axios.get(firmwareLookup);
 
-    const baseLink = data["baseLink"];
-    const fileName = data[firmwareVersion]["fileName"];
+  const baseLink = data["baseLink"];
+  const fileName = data[firmwareVersion]["fileName"];
 
-    const firmwareLink = `${baseLink}${fileName}`;
-    console.log(`Downloading firmware from ${firmwareLink}`);
+  const firmwareLink = `${baseLink}${fileName}`;
+  log.info(`Downloading firmware from ${firmwareLink}`);
 
-    return new Promise((resolve, reject) => {
-        axios({
-            url: firmwareLink,
-            method: "GET",
-            responseType: "stream",
-        }).then((response) => {
-            // Use the stream to write the file to the filesystem
-            response.data.pipe(fs.createWriteStream(FIRMWARE_PATH));
-            response.data.on("end", () => {
-                console.log("Download Completed");
-                resolve();
-            });
-        });
-    });
+  return new Promise((resolve, reject) => {
+    axios({
+      url: firmwareLink,
+      method: "GET",
+      responseType: "stream",
+    }).then((response) => {
+      // Use the stream to write the file to the filesystem
+      response.data.pipe(fs.createWriteStream(FIRMWARE_PATH));
+      response.data.on("end", () => {
+        log.info("Download Completed");
+        resolve();
+      });
+    }).catch(
+      function(error) {
+        if (error.response) {
+          // Request made and server responded
+          log.error(error.response.data);
+          log.error(error.response.status);
+          log.error(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          log.error(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          log.error('Error', error.message);
+        }
+
+      }
+    );
+  });
+
 }
 
 async function getVersionLookup() {
-    const versionLookup =
-        "https://raw.githubusercontent.com/Kuromis-2/newest-firmware/main/versionlookup.json";
+  const versionLookup =
+    "https://raw.githubusercontent.com/Kuromis-2/newest-firmware/main/versionlookup.json";
 
-    // Fetch data from versionLookup with a https request and return JSON
-    return new Promise((resolve, reject) => {
-        https.get(versionLookup, (response) => {
-            let data = "";
-            response.on("data", (chunk) => {
-                data += chunk;
-            });
-            response.on("end", () => {
-                resolve(JSON.parse(data));
-            });
-        });
+  // Fetch data from versionLookup with a https request and return JSON
+  return new Promise((resolve, reject) => {
+    https.get(versionLookup, (response) => {
+      let data = "";
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+      response.on("end", () => {
+        resolve(JSON.parse(data));
+      });
     });
+  });
 }
 
 async function getComPorts() {
-    console.log("Retrieving all Serial Ports...");
-    let ports = [];
-    let portList = await SerialPort.list();
-    for (const port of portList) {
-        ports.push(port);
-        console.log(`Port: ${port.path}`);
-    }
+  console.log("Retrieving all Serial Ports...");
+  let ports = [];
+  let portList = await SerialPort.list();
+  for (const port of portList) {
+    ports.push(port);
+    console.log(`Port: ${port.path}`);
+  }
 
-    console.log(`Found ${ports.length} ports!`);
-    return ports;
+  console.log(`Found ${ports.length} ports!`);
+  return ports;
 }
 
 function getNewlyPluggedInPorts(beforePorts, afterPorts) {
-    if (beforePorts == null) throw new Error("beforePorts is null");
-    if (afterPorts == null) throw new Error("afterPorts is null");
-    if (afterPorts.length < beforePorts.length)
-        throw new Error("afterPorts.length < beforePorts.length");
-    if (afterPorts.length === 0) throw new Error("afterPorts.length === 0");
+  if (beforePorts == null) throw new Error("beforePorts is null");
+  if (afterPorts == null) throw new Error("afterPorts is null");
+  if (afterPorts.length < beforePorts.length)
+    throw new Error("afterPorts.length < beforePorts.length");
+  if (afterPorts.length === 0) throw new Error("afterPorts.length === 0");
 
-    // Save only newly plugged in ports and use JSON.stringyfy to compare
-    const newPorts = [];
-    for (const port of afterPorts) {
-        if (beforePorts.find((p) => p.path === port.path) == null) {
-            newPorts.push(port);
-        }
+  // Save only newly plugged in ports and use JSON.stringyfy to compare
+  const newPorts = [];
+  for (const port of afterPorts) {
+    if (beforePorts.find((p) => p.path === port.path) == null) {
+      newPorts.push(port);
     }
+  }
 
-    return newPorts;
+  return newPorts;
 }
 
 async function updateMouseHelper(pluggedInPorts, firmwareVersion) {
-    if (Object.keys(pluggedInPorts).length === 0) {
-        throw "No new com ports plugged in empty object";
-    }
+  if (Object.keys(pluggedInPorts).length === 0) {
+    throw "No new com ports plugged in empty object";
+  }
 
-    // If there are no plugged in ports, return
-    if (pluggedInPorts.length === 0) {
-        throw "No new com ports plugged in empty array";
-    }
+  // If there are no plugged in ports, return
+  if (pluggedInPorts.length === 0) {
+    throw "No new com ports plugged in empty array";
+  }
 
-    // If there is more than one plugged in port console error and return
-    if (pluggedInPorts.length > 1) {
-        throw "More than one com port plugged in";
-    }
+  // If there is more than one plugged in port console error and return
+  if (pluggedInPorts.length > 1) {
+    throw "More than one com port plugged in";
+  }
 
-    // If there is only one plugged in port, update the mouse
-    if (pluggedInPorts.length === 1) {
-        const port = pluggedInPorts[0];
-        await downloadFirmware(firmwareVersion);
-        console.log(
-            `Started update process with firmware version: ${firmwareVersion}`
-        );
+  // If there is only one plugged in port, update the mouse
+  if (pluggedInPorts.length === 1) {
+    const port = pluggedInPorts[0];
+    await downloadFirmware(firmwareVersion);
+    console.log(
+      `Started update process with firmware version: ${firmwareVersion}`
+    );
 
-        try {
-            await updateMouse(port, FIRMWARE_PATH);
-            console.log("Finished update process");
-            console.log("Success");
-        } catch (e) {
-            console.log("Finished update process");
-            console.log("Failure");
-            throw e;
-        }
+    try {
+      await updateMouse(port, FIRMWARE_PATH);
+      console.log("Finished update process");
+      console.log("Success");
+    } catch (e) {
+      console.log("Finished update process");
+      console.log("Failure");
+      throw e;
     }
+  }
 }
 
 // export getComPorts, updateMouseHelper
 module.exports = {
-    getVersionLookup,
-    getComPorts,
-    getNewlyPluggedInPorts,
-    updateMouseHelper,
+  getVersionLookup,
+  getComPorts,
+  getNewlyPluggedInPorts,
+  updateMouseHelper,
 };
--- src/main.js
+++ src/main.js
@@ -1,107 +1,146 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const log = require('electron-log');
 const ipcMain = require("electron").ipcMain;
+const fs = require('fs');
+const os = require('os');
 const {
-    getVersionLookup,
-    getComPorts,
-    getNewlyPluggedInPorts,
-    updateMouseHelper,
+  getVersionLookup,
+  getComPorts,
+  getNewlyPluggedInPorts,
+  updateMouseHelper,
 } = require("./firmwarehelpers.js");
 
-console.log("main.js loaded");
-function createWindow() {
-    console.log("createWindow got executed");
 
-    // https://github.com/electron/electron/issues/13670
-    let windowWidth = 453;
-    let windowHeight = 453;
-
-    if (process.platform === "win32") {
-        // Windows and Mac had some window size issues
-        windowWidth += 5;
-        windowHeight += 23;
-    } else if (process.platform === "darwin") {
-        windowWidth -= 1;
-        windowHeight += 22;
+function getAppDataPath() {
+  switch (process.platform) {
+    case "darwin": {
+      return path.join(process.env.HOME, "Library", "Application Support", "Finalware");
     }
-
-    const debug = false;
-
-    // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: windowWidth,
-        height: windowHeight,
-        webPreferences: {
-            devTools: !app.isPackaged,
-            preload: `${__dirname}/preload.js`,
-        },
-        enableRemoteModule: true,
-        nodeIntegration: true,
-        autoHideMenuBar: true,
-        resizable: debug,
-        maximizable: debug,
-        icon: `${__dirname}/../app/finalmouse-logo.ico`,
-    });
-
-    // and load the index.html of the app.
-    console.log("Loading index.html");
-    mainWindow.loadFile("app/index.html");
-    console.log("index.html loaded");
-
-    if (!app.isPackaged) {
-        mainWindow.setAlwaysOnTop(true, "screen");
+    case "win32": {
+      return path.join(process.env.APPDATA, "Finalware");
     }
+    case "linux": {
+      return path.join(process.env.HOME, ".Finalware");
+    }
+    default: {
+      log.error("Unsupported platform!");
+      process.exit(1);
+    }
+  }
 }
+const APP_DATA = getAppDataPath();
+
+log.transports.file.resolvePath = () => path.join(APP_DATA, 'logs/main.log');
+log.info(`Log file gets saved to ${path.join(APP_DATA, 'logs/main.log')}`)
+log.info("main.js loaded and modules imported");
+
+
+log.info(`${arguments.callee.name} got executed`);
+log.info(`Platform: ${process.platform} version ${os.release()}`);
+log.info(`App is ${!app.isPackaged ? "not" : ""} packaged`);
+
+
+
+
+log.transports.file.resolvePath = () => path.join(APP_DATA, 'logs/main.log');
+
+
+function createWindow() {
+  // https://github.com/electron/electron/issues/13670
+  let windowWidth = 453;
+  let windowHeight = 453;
+
+  if (process.platform === "win32") {
+    // Windows and Mac had some window size issues
+    windowWidth += 5;
+    windowHeight += 23;
+  } else if (process.platform === "darwin") {
+    windowWidth -= 1;
+    windowHeight += 22;
+  }
+
+  const debug = false;
+
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: windowWidth,
+    height: windowHeight,
+    webPreferences: {
+      devTools: !app.isPackaged,
+      preload: `${__dirname}/preload.js`,
+    },
+    enableRemoteModule: true,
+    nodeIntegration: true,
+    autoHideMenuBar: true,
+    icon: `${__dirname}/../app/finalmouse-logo.ico`,
+
+  });
+
+  // and load the index.html of the app.
+  log.info("Loading index.html");
+  mainWindow.loadFile("app/index.html");
+  log.info("Loaded index.html");
+
+  if (!app.isPackaged) {
+    mainWindow.setAlwaysOnTop(true, "screen");
+  }
+
+}
+
+
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-    console.log("App is ready!");
-    createWindow();
-    ipcMain.handle("exit", (event, title) => {
-        app.quit();
-    });
+  //log the status of the app with electron-log
+  log.info("app.whenReady got executed");
+  createWindow();
+  ipcMain.handle("exit", (event, title) => {
+    app.quit();
+  });
 
-    ipcMain.handle("get-versions", async (event) => {
-        return await getVersionLookup();
-    });
+  ipcMain.handle("get-versions", async (event) => {
+    return await getVersionLookup();
+  });
 
-    ipcMain.handle("get-ports", async (event) => {
-        return await getComPorts();
-    });
+  ipcMain.handle("get-ports", async (event) => {
+    return await getComPorts();
+  });
 
-    ipcMain.handle("get-new-ports", async (event, beforePorts, afterPorts) => {
-        return await getNewlyPluggedInPorts(
-            JSON.parse(beforePorts),
-            JSON.parse(afterPorts)
-        );
-    });
-
-    ipcMain.handle(
-        "update-mouse",
-        async (event, pluggedInPorts, firmwareVersion) => {
-            return await updateMouseHelper(
-                JSON.parse(pluggedInPorts),
-                firmwareVersion
-            );
-        }
+  ipcMain.handle("get-new-ports", async (event, beforePorts, afterPorts) => {
+    return await getNewlyPluggedInPorts(
+      JSON.parse(beforePorts),
+      JSON.parse(afterPorts)
     );
+  });
 
-    app.on("activate", function () {
-        // On macOS it's common to re-create a window in the app when the
-        // dock icon is clicked and there are no other windows open.
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+  ipcMain.handle(
+    "update-mouse",
+    async (event, pluggedInPorts, firmwareVersion) => {
+      return await updateMouseHelper(
+        JSON.parse(pluggedInPorts),
+        firmwareVersion
+      );
+    }
+  );
+
+  app.on("activate", function() {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
 try {
-    require("electron-reloader")(module);
-} catch (_) {}
+  require("electron-reloader")(module);
+} catch (_) { }
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-    if (process.platform !== "darwin") app.quit();
+app.on("window-all-closed", function() {
+  if (process.platform !== "darwin") app.quit();
 });
 //ipc function that exits the app
